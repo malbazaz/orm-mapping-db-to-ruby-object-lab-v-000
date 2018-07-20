a0dec373aacd202547492ff91e0857c0a500026e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-20 16:02:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,8 +79,10 @@ class Student
       FROM students
       WHERE grade <=11;
     SQL
- name_something= DB[:conn].execute(sql)
-    binding.pry
+  DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+  end 
+    
   end
 
   def self.first_X_students_in_grade_10(x)
